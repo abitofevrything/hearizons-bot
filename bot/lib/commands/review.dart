624c0d456559a4513c96b36ef6f9e74c2f5ebd24
Abitofevrything <mylo.fawcett@gmail.com>
2022-11-14 19:19:19 +0000

    Use modals for getting reviews

--- bot/lib/commands/review.dart
+++ bot/lib/commands/review.dart
@@ -3,6 +3,7 @@ import 'package:hearizons/commands/converters/assignment.dart';
 import 'package:hearizons/database/database.dart';
 import 'package:hearizons/utils/context_extension.dart';
 import 'package:nyxx_commands/nyxx_commands.dart';
+import 'package:nyxx_interactions/nyxx_interactions.dart';
 
 final review = ChatCommand(
   'review',
@@ -10,16 +11,24 @@ final review = ChatCommand(
   id(
     'review',
     (
-      IChatContext context,
+      InteractionChatContext context,
       @Description('The assignment to review')
       @UseConverter(assignedAssignmentConverter)
           Assignment assignment,
-      @Description('The content of the review') String review,
     ) async {
       final database = GetIt.I.get<Database>();
       final event = await database.getEventFromAssignment(assignment);
 
-      await event.createReview(assignment, review);
+      final modal = await context.getModal(
+        title: 'Review',
+        components: [
+          TextInputBuilder('content', TextInputStyle.paragraph, 'Your review')
+            ..maxLength = 1024
+            ..minLength = 50,
+        ],
+      );
+
+      await event.createReview(assignment, modal['content']);
 
       await context.success(
         title: 'Review created',
